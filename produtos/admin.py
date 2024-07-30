f0b5048cff85213af1bc12afcf5e947570844407
Dante-Lucas <157182980+Dante-Lucas@users.noreply.github.com>
2024-07-30 08:02:13 -0300

    Delete produtos directory

--- produtos/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Produto,Fabricante,Categoria
-# Register your models here.
-admin.site.register(Categoria)
-admin.site.register(Fabricante)
-@admin.register(Produto)
-class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ['id','nome_produto','estoque','categoria','preco','fabricante','categoria']
-    list_filter = ['fabricante','categoria']
